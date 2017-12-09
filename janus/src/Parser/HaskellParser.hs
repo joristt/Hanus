a01dcc6040e0e715e5ba059aede55dd5197b5487
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2017-12-09 15:32:22 +0100

    Add Haskell parser

---
+++ janus/src/Parser/HaskellParser.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE FlexibleContexts, Rank2Types #-}
+module Parser.HaskellParser where
+
+import Data.Either.Extra
+
+import Text.ParserCombinators.UU
+import Text.ParserCombinators.UU.Core
+import Text.ParserCombinators.UU.BasicInstances
+import Language.Haskell.Meta.Parse
+import Language.Haskell.TH.Syntax
+
+myTest = parse (pType [";"]) (createStr (LineColPos 0 0 0) "Maybe [a;")
+
+pType :: [String] -> Parser (Type, String)
+pType splits = pSplitUntilSuccess "" splits (eitherToMaybe . parseType . init)
+
+pExp :: [String] -> Parser (Exp, String)
+pExp splits = pSplitUntilSuccess "" splits (eitherToMaybe . parseExp . init)
+
+pSplitUntilSuccess :: String -> [String] -> (String -> Maybe a) -> Parser (a, String)
+pSplitUntilSuccess prefix splits f = do
+  (str, sep) <- pUntil splits
+  case f (prefix ++ str) of
+    Just a  -> return (a, sep)
+    Nothing -> pSplitUntilSuccess (prefix ++ str ++ sep) splits f
+
+pUntil :: [String] -> Parser (String, String)
+pUntil splits = pSplit <<|> (\x (y, z) -> (x : y, z)) <$> pRange (minBound, maxBound) <*> pUntil splits
+  where
+    pSplit = (\x -> ([], x)) <$> pAny pToken splits
