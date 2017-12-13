953df8903e9fdef156c89590a5bbcab7d5da564d
Cas van der Rest <casmb11@xs4all.nl>
2017-12-13 13:04:57 +0100

    global pattern gen

--- janus/src/Eval.hs
+++ janus/src/Eval.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell #-} 
 
 module Eval where
 
@@ -7,44 +7,27 @@ import Language.Haskell.TH.Syntax
 import Language.Haskell.TH
 import Data.Maybe
 
-typeDefault :: AST.Type -> Lit
-typeDefault Int    = IntegerL 0
-typeDefault String = StringL ""
-
-declare :: Name -> Lit -> Stmt
-declare n l = LetS [ValD (VarP n) (NormalB (LitE l)) []]
-
---evalDeclaration :: GlobalVarDeclaration -> 
-evalDeclaration (GlobalVarDeclaration (Variable (Identifier name) t) _) = do
-    name <- newName name
-    let bind = declare name (typeDefault t)
-    let ret = NoBindS (VarE name)
-    return $ DoE [bind, ret]
-
-getVal :: Integer -> Q Exp
-getVal value = do
-    name <- newName "a"
-    let bind = LetS [ValD (VarP name) (NormalB (LitE (IntegerL value))) []]
-    let ret  = NoBindS (VarE name)
-    let qdo  = DoE [bind, ret]
-    return qdo
-
 evalProgram :: Program -> Q [Dec]
 evalProgram (Program decls) = do  
         x <- entry
-        return x:[]
+        fdecs <- mapM (evalProcedure statePattern) procedures
+        return (x:fdecs)
     where globalVars = filter filterVars decls
-          procedures = filter filterProcs decls
-          filterVars dec  = case dec of 
-                                GlobalVarDeclaration _ _ -> True
-                                otherwise                -> False
-          filterProcs dec = case dec of 
-                                Procedure _ _ _ -> True
-                                otherwise       -> False
-          entry = do 
-              unit <- [e|()|]
-              let body = (DoE [NoBindS unit])
-              return (FunD (mkName "run") [Clause [] (NormalB body) []])
+        procedures = filter filterProcs decls
+        filterVars dec  = case dec of 
+                              GlobalVarDeclaration _ _ -> True
+                              otherwise                -> False
+        filterProcs dec = case dec of 
+                              Procedure _ _ _ -> True
+                              otherwise       -> False
+        entry = do 
+            unit <- [e|()|]
+            let body = (DoE [NoBindS unit])
+            return (FunD (mkName "run") [Clause [] (NormalB body) []])
+
+statePattern :: [GlobalVarDeclaration] -> Q [Pat]
+statePattern varDecs = patterns (map toPat varDecs)
+    where toPat (GlobalVarDeclaration var _) = varToPat var
 
 shadow :: Identifier -> Q Name
 shadow (Identifier n) = newName n
@@ -58,5 +41,3 @@ evalProcedure (Procedure n vs b) = do
     body <- evalBlock s
     let args = map VarP vs
     FunD name [Clause args body []]
-
-evalBlock stmts s = do
