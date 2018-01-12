e2ded07dea7d831609a05518dae657d351b055c8
Orestis Melkonian <melkon.or@gmail.com>
2018-01-12 19:17:23 +0100

    Benchmarking setup

--- janus/app/Main.hs
+++ janus/app/Main.hs
@@ -1,34 +1,64 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE DoAndIfThenElse #-}
+{-# LANGUAGE TemplateHaskell, ScopedTypeVariables, QuasiQuotes, FlexibleContexts #-}
 
 module Main where
 
-import Options.Applicative
+import Criterion.Main
+import Control.Monad
 import Data.Semigroup ((<>))
-import Parser.JanusParser
+import Options.Applicative
+import System.Environment (withArgs)
+import Parser.JanusParser (parser)
 
-import Parser.JanusParser
+import QQ
+import StdLib.Operator
+import StdLib.DefaultValue
 
 -- | Command-line options.
-newtype Options = Options { inputFile :: String }
+data Options = Options
+  { input     :: Int
+  , benchmark :: Bool
+  , output    :: String
+  }
 
 -- | Parsing of command-line options.
 parseOptions :: Parser Options
 parseOptions = Options
-  <$> strOption
-      (  long "input"
-      <> short 'i'
-      <> metavar "STRING"
+  <$> option auto
+      (  short 'n'
+      <> metavar "INT"
       <> help "Janus source file"
       )
+  <*> switch
+      (  short 'b'
+      <> long "benchmark"
+      <> help "Run benchmark"
+      )
+  <*> option auto
+      (  short 'o'
+      <> long "output"
+      <> showDefault
+      <> value "report.html"
+      <> metavar "STR"
+      <> help "Output file for benchmark"
+      )
+
+withInfo :: Parser a -> String -> ParserInfo a
+withInfo opts desc = info (helper <*> opts) $ progDesc desc
 
 -- | Main.
 main :: IO ()
-main = run =<< execParser (parseOptions `withInfo` "Janus DSL")
-  where
-    withInfo :: Parser a -> String -> ParserInfo a
-    withInfo opts desc = info (helper <*> opts) $ progDesc desc
-    run :: Options -> IO ()
-    run (Options inputFile) = do
-      prog <- readFile inputFile
-      print $ parser "" 0 0 prog
+main = runMain =<< execParser (parseOptions `withInfo` "Janus DSL")
+
+[hanusTF|examples/testrle.janus|]
+
+-- | Run.
+runMain :: Options -> IO ()
+runMain (Options input benchmark output) =
+  if benchmark then
+    withArgs ["-o", output] $
+      defaultMain [bgroup "t" [bench "1" $ whnfIO go]]
+  else go
+  where go = print $ hanus_main (replicate 32 1) []
+    -- forM_ [1..1000] (\_ -> do let (_, z) = hanus_main (replicate 32 1) []
+       --                                print $ main' [] z)
--- janus/examples/fibonacci.hs
+++ janus/examples/fibonacci.hs
@@ -7,7 +7,7 @@ import QQ
 import StdLib.Operator
 import StdLib.DefaultValue
 
-[hanusT| 
+[hanusT|
 procedure fib(x1 :: Int, x2 :: Int, n :: Int)
 {
     if n == 0 then
@@ -19,7 +19,7 @@ procedure fib(x1 :: Int, x2 :: Int, n :: Int)
         x1 += x2;
         swap x1 x2;
     fi x1 == x2;
-    
+
 }
 
 procedure main(x1 :: Int, x2 :: Int, n :: Int){
