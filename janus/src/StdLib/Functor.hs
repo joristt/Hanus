4135436fc19b272039b11038a58e3dcbf22bf95e
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2018-01-23 14:24:53 +0100

    Add emoji aliasses 🏦

--- janus/src/StdLib/Functor.hs
+++ janus/src/StdLib/Functor.hs
@@ -7,18 +7,22 @@ import StdLib.DefaultValue
 -- Example:
 -- x :: Int[];
 -- x =$ complement;
-(=$) :: Functor f => Operator (f a) (Operator a ())
+(=$), (🏦) :: Functor f => Operator (f a) (Operator a ())
 (=$) = Operator g h
   where
     g f (Operator fwd _) = fmap (`fwd` ()) f
     h f (Operator _ bwd) = fmap (`bwd` ()) f
 
+(🏦) = (=$)
+
 -- Example:
 -- x :: Int[];
 -- x =$$ (+=, 1);
 -- (The inverse of this statement is `x =$$ (-=, 1);`)
-(=$$) :: Functor f => Operator (f a) (Operator a b, b)
+(=$$), (🏦🏦) :: Functor f => Operator (f a) (Operator a b, b)
 (=$$) = Operator g h
   where
     g f (Operator fwd _, arg) = fmap (`fwd` arg) f
     h f (Operator _ bwd, arg) = fmap (`bwd` arg) f
+
+(🏦🏦) = (=$$)
