4ad834e5d4fd1f204c55e71c8195a6e011be0304
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2018-01-25 15:21:59 +0100

    Fix operators

--- janus/src/StdLib/Operator.hs
+++ janus/src/StdLib/Operator.hs
@@ -6,22 +6,22 @@ import qualified Data.Map.Strict as M
 
 data Operator a b = Operator (a -> b -> a) (a -> b -> a)
 
-inverseOf :: Operator a b -> (a -> b -> a) -> Operator a b
-inverseOf (Operator f _) g = Operator g f
+inverse :: Operator a b -> Operator a b
+inverse (Operator f g) = Operator g f
 
 symmetric :: (a -> b -> a)-> Operator a b
 symmetric f = Operator f f
 
 (+=), (-=) :: (Num a) => Operator a a
 (+=) = Operator  (+) (-)
-(-=) = inverseOf (+=) (-)
+(-=) = inverse (+=)
 
 (^=) :: (B.Bits a) => Operator a a
-(^=) = inverseOf (^=) B.xor
+(^=) = symmetric B.xor
 
 -- Usage: complement x;
 complement :: (B.Bits a) => Operator a ()
-complement = inverseOf complement (\x () -> B.complement x)
+complement = symmetric (\x () -> B.complement x)
 
 -- Usage: swap x y;
 swap :: Operator (a, a) ()
@@ -29,26 +29,27 @@ swap = symmetric (\(x, y) () -> (y, x))
 
 push, pop :: (DefaultValue a, Eq a) => Operator ([a], a) ()
 
-push = inverseOf pop (\(stack, val) _ -> (val : stack, defaultValue))
-pop  = inverseOf push f
+push = Operator (\(stack, val) _ -> (val : stack, defaultValue)) pop'
   where
-    f (s:stack, value) _
+    pop' (s:stack, value) _
       | value == defaultValue = (stack, s)
       | otherwise = error "pop: Second argument is not the default value"
-    f ([], _) _ = error "pop: Stack is empty"
+    pop' ([], _) _ = error "pop: Stack is empty"
+
+pop = inverse push
 
 mapAdd :: (Ord k, Eq v, DefaultValue v) => Operator (M.Map k v) k
-mapAdd = inverseOf mapRemove f
+mapAdd = Operator add rm
   where
-    f m key = case M.lookup key m of
+    add m key = case M.lookup key m of
       Nothing -> M.insert key defaultValue m
       _ -> error "mapAdd: key already exists in this Map"
-
-mapRemove :: (Ord k, Eq v, DefaultValue v) => Operator (M.Map k v) k
-mapRemove = inverseOf mapAdd f
-  where
-    f m key = case M.lookup key m of
+    rm m key = case M.lookup key m of
       Nothing -> error "mapRemove: key does not exist in this Map"
       (Just x)
         | x == defaultValue -> M.delete key m
         | otherwise -> error "mapRemove: value in Map is not equal to defaultValue"
+  
+
+mapRemove :: (Ord k, Eq v, DefaultValue v) => Operator (M.Map k v) k
+mapRemove = inverse mapAdd
